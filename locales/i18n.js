cb7eefef809b30275a8006a04b20fdfe841e5ba7
Marek Sommer <marek.sommer.1@gmail.com>
2024-02-22 22:41:59 +0100

    Add i18n setup

---
+++ locales/i18n.js
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import en from "@/locales/en.json";
+import cs from "@/locales/cs.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(Backend)
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    resources: {
+      en: {
+        translation: { ...en },
+      },
+      cs: {
+        translation: { ...cs },
+      },
+    },
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
