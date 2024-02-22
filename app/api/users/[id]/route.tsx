3cbdf294dd7aac0203c97ab529748eeb5d2cff3b
Marek Sommer <marek.sommer.1@gmail.com>
2024-02-22 22:43:12 +0100

    Add translatoins

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,16 +1,17 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { t } from "@/locales/translate";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   if (!params || !params.id)
-    return NextResponse.json({ status: 400, message: "ID is required" });
+    return NextResponse.json({ status: 400, message: t("IdIsRequired") });
 
   const user = await prisma.user.findUnique({ where: { id: params.id } });
   if (!user)
-    return NextResponse.json({ status: 404, message: "User not Found" });
+    return NextResponse.json({ status: 404, message: t("UserNotFound") });
 
   return NextResponse.json(user);
 }
