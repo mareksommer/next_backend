f0c519a8fd9cfc0cdb5af31f028ca0de717808de
Marek Sommer <marek.sommer.1@gmail.com>
2024-02-24 23:35:21 +0100

    MOve validation user schema into separate file

---
+++ models/user/validation.schema.ts
@@ -0,0 +1,20 @@
+import { ZodIssue, z } from "zod";
+
+export const postSchema = z.object({
+  email: z.string().email(),
+  firstName: z.string().nullable(),
+  lastName: z.string().nullable(),
+  password: z.string().min(6),
+});
+
+export const patchSchema = z.object({
+  id: z.string(),
+  email: z.string().email().optional(),
+  firstName: z.string().nullable().optional(),
+  lastName: z.string().nullable().optional(),
+  password: z.string().min(6).optional(),
+});
+
+export const idSchema = z.object({
+  id: z.string(),
+});
