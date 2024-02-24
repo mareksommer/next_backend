e048d897390c0cd43f08704b2837b8658cde661d
Marek Sommer <marek.sommer.1@gmail.com>
2024-02-24 23:37:44 +0100

    Add return types

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,24 +1,24 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { processRequest } from "@/services/request";
 import { getUser, updateUser, deleteUser } from "@/models/user/user";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
-) {
+): Promise<NextResponse> {
   return processRequest(getUser, { id: params.id });
 }
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
-) {
-  return processRequest(updateUser, {request: request, id: params.id});
+): Promise<NextResponse> {
+  return processRequest(updateUser, { request: request, id: params.id });
 }
 
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
-) {
+): Promise<NextResponse> {
   return processRequest(deleteUser, { id: params.id });
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { getUsers, createUser } from "@/models/user/user";
 import { processRequest } from "@/services/request";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest): Promise<NextResponse> {
   return processRequest(getUsers, request);
 }
 
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest): Promise<NextResponse> {
   return processRequest(createUser, request);
 }
