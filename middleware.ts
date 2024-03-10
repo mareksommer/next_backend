09c65b28eb06be1cc8aae15379fce1b96c2ecb38
Marek Sommer <marek.sommer.1@gmail.com>
2024-03-10 23:07:25 +0100

    Add auth middleware

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { t } from "@/locales/translate";
+import { verifyToken } from "@/services/auth";
+
+const excludedPaths = ["/api/auth"];
+/*
+ * Middleware to verify the token
+ */
+export function middleware(request: NextRequest) {
+  if (excludedPaths.includes(request.nextUrl.pathname))
+    return NextResponse.next();
+
+  const authToken = request.headers.get("x-auth-token");
+  if (!authToken)
+    return NextResponse.json({
+      status: 401,
+      message: t("Access denied. No token provided."),
+    });
+
+  const verifiedToken = verifyToken(authToken);
+  if (!verifiedToken)
+    return NextResponse.json({
+      status: 401,
+      message: t("Access denied. Invalid token."),
+    });
+}
+
+/*
+ * Match all request paths for api routes
+ */
+export const config = {
+  matcher: "/((?api).*)",
+};
