1435b860e550aca57edb5ae81c3f1722e2c27772
Marek Sommer <marek.sommer.1@gmail.com>
2024-03-10 22:37:11 +0100

    Remove has service - replaced by auth

--- services/hash.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const getHashFromString = async (stringToHash: string): Promise<string> => {
-  const salt = await bcrypt.genSalt(10)
-  return bcrypt.hash(stringToHash, salt)
-}
