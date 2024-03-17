ac21af5637a50e2560af40ea82d7073450f8535b
Marek Sommer <marek.sommer.1@gmail.com>
2024-03-17 15:56:05 +0100

    Add isExcludedPath check

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,29 @@
 import '@/locales/i18n';
 import { t } from "@/locales/translate";
 import { verifyToken } from "@/services/auth";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-const excludedPaths = ["/api/auth"];
+const excludedPaths = [
+  { route: "/api/auth", methods: ["POST"] },
+  { route: "/api/lost-password", methods: ["POST"] },
+  { route: "/api/reset-password", methods: ["POST"] },
+  { route: "/api/register", methods: ["POST"] },
+];
+
+function isExcludedPath(request: NextRequest) {
+  return excludedPaths.some(
+    (path) =>
+      path.route === request.nextUrl.pathname &&
+      path.methods.includes(request.method)
+  );
+}
+
 /*
  * Middleware to verify the token
  */
 export function middleware(request: NextRequest) {
-  if (excludedPaths.includes(request.nextUrl.pathname))
+  if (isExcludedPath(request))
     return NextResponse.next();
 
   const authToken = request.headers.get("x-auth-token");
