e88597adc644b31244cedb91934953d9208aadf3
Marek Sommer <marek.sommer.1@gmail.com>
2024-03-17 19:04:21 +0100

    Ad email service

---
+++ services/email.ts
@@ -0,0 +1,20 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.SENDGRID_API_KEY);
+
+interface EmailOptions {
+  from?: string;
+  to?: string;
+  subject: string;
+  react: JSX.Element;
+}
+
+export async function send(options: EmailOptions) {
+  const { from, to, subject, react } = options;
+  resend.emails.send({
+    from: from || process.env.EMAIL_FROM!,
+    to: to || process.env.EMAIL_TO!,
+    subject: subject,
+    react: react
+  });
+}
