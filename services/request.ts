30e27c7b950ce66ccbbcddea1b998398ff262eea
Marek Sommer <marek.sommer.1@gmail.com>
2024-06-15 23:21:11 +0200

    Update request service to return status as http status code

--- services/request.ts
+++ services/request.ts
@@ -1,20 +1,29 @@
 import { NextResponse } from "next/server";
 import { t } from "@/locales/translate";
 
-export const processRequest = async (callback: Function, args: Object | null): Promise<NextResponse> => {
+export const processRequest = async (
+  callback: Function,
+  args: Object | null
+): Promise<NextResponse> => {
   try {
     const result = await callback(args);
-    return NextResponse.json(result);
+    return NextResponse.json(result.body, { status: result.status });
+  } catch (error: any) {
+    // TODO add error logging
+    const errorMessage = error.message
+      ? error.message
+      : t("Something went wrong");
+    return NextResponse.json(
+      { errors: [{ message: errorMessage }] },
+      { status: 500 }
+    );
   }
-  catch (error: any) {
-    // TODO add erorr logging
-    const errorMessage = (error.message) ? error.message : t('Something went wrong');
-    return NextResponse.json({ status: 500, message: errorMessage});
-  }
-}
+};
 
-export const getPagination = (URLSearchParams: URLSearchParams): { take: number, skip: number } => {
+export const getPagination = (
+  URLSearchParams: URLSearchParams
+): { take: number; skip: number } => {
   const take = URLSearchParams.get("take") || "10";
   const skip = URLSearchParams.get("skip") || "0";
   return { take: parseInt(take), skip: parseInt(skip) };
-}
+};
